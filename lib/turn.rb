2e4abf34c0e08b88fe053b2a3e10b61b5950bcc2
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-07-08 20:05:29 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,7 +23,11 @@ def valid_move?(board, index, player)
 end
 
 def position_taken?(board, index)
-  
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
++    false
++  elsif board[index] == "X" || board[index] == "O"
++    true
++  end
 end
 
 def move(board, position, player="X")
